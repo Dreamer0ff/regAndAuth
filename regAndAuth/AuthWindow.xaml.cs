6a7160df3fbc6bcedb49f66339fba83a620ff590
Dreamer0ff <101452823+Dreamer0ff@users.noreply.github.com>
2024-03-10 17:17:00 +0300

    Добавьте файлы проекта.

---
+++ regAndAuth/AuthWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace regAndAuth
+{
+    /// <summary>
+    /// Логика взаимодействия для AuthWindow.xaml
+    /// </summary>
+    public partial class AuthWindow : Window
+    {
+        public AuthWindow()
+        {
+            InitializeComponent();
+        }
+        private void Login_Click(object sender, RoutedEventArgs e)
+        {
+            SqlConnection sqlCon = new SqlConnection(@"Data Source=DESKTOP-8UCIOCV\SQLEXPRESS;Initial Catalog=regAndAuth;Integrated Security=True;");
+            try
+            {
+                if (sqlCon.State == ConnectionState.Closed)
+                    sqlCon.Open();
+                String query = "SELECT name, surname FROM authData WHERE login=@login AND password=@password";
+                SqlCommand sqlCmd = new SqlCommand(query, sqlCon);
+                sqlCmd.CommandType = CommandType.Text;
+                sqlCmd.Parameters.AddWithValue("@login", userLoginLog.Text);
+                sqlCmd.Parameters.AddWithValue("@password", userPasswordLog.Password);
+
+                SqlDataReader reader = sqlCmd.ExecuteReader();
+
+                if (reader.Read())
+                {
+                    string firstName = reader.GetString(0);
+                    string lastName = reader.GetString(1);
+                    Window1 dashboard = new Window1(firstName, lastName);
+                    dashboard.Show();
+                    this.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Username or password is incorrect.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                sqlCon.Close();
+            }
+        }
+    }
+}
---
+++ regAndAuth/MainWindow.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace regAndAuth
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void RegisterButton_Click(object sender, RoutedEventArgs e)
+        {
+            string firstName = userName.Text;
+            string lastName = userSurname.Text;
+            string username = userLogin.Text;
+            string password = userPassword.Password;
+
+            authData newauthData = new authData
+            {
+                FirstName = firstName,
+                LastName = lastName,
+                Username = username,
+                Password = password
+            };
+
+            newauthData.RegisterUser();
+
+            AuthWindow authWindow = new AuthWindow();
+            authWindow.Show();
+        }
+    }
+
+    public class authData
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        public void RegisterUser()
+        {
+            string connectionString = "Data Source=DESKTOP-8UCIOCV\\SQLEXPRESS;Initial Catalog=regAndAuth;Integrated Security=True;";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                string query = "INSERT INTO authData (name, surname, login, password) VALUES (@FirstName, @LastName, @Username, @Password)";
+
+                SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@FirstName", FirstName);
+                command.Parameters.AddWithValue("@LastName", LastName);
+                command.Parameters.AddWithValue("@Username", Username);
+                command.Parameters.AddWithValue("@Password", Password);
+
+                command.ExecuteNonQuery();
+            }
+        }
+    }
+    public class User
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+    public class LoginTime
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
+
